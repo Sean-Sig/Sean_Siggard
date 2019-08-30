34e01636650e789881d2518259338da42bb50d00
sean-sig <ssiggard@apio.tech>
2019-08-29 18:36:32 -0600

    boxes

---
+++ src/components/Box.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Project
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </a>
+      </header>
+      <div class="content">
+        <img src="https://cdnph.upi.com/svc/sv/upi/5581566909407/2019/1/127b3c18cb2ae973bc292527ae956fb4/LeBron-James-praises-Ken-Griffey-Jrs-greatest-swing-ever.jpg" alt="Italian Trulli">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
